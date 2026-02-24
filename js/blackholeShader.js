c7cc466ec53c39b2eabad9406205680ced301d9a
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-24 23:46:22 +0530

    Update blackholeShader.js

--- js/blackholeShader.js
+++ js/blackholeShader.js
@@ -1,17 +1,29 @@
-import blackholeFrag from './blackholeLens.glsl?raw';
+// js/blackholeShader.js
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 
 export const blackHoleShader = {
   uniforms: {
     tDiffuse: { value: null },
-    blackHolePos: { value: new THREE.Vector3(0,0,0) },
-    strength: { value: 0.2 }
+    blackHolePos: { value: new THREE.Vector3(0, 0, 0) },
+    strength: { value: 0.25 }
   },
   vertexShader: `
     varying vec2 vUv;
-    void main(){
+    void main() {
       vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
   `,
-  fragmentShader: blackholeFrag
+  fragmentShader: `
+    uniform sampler2D tDiffuse;
+    uniform float strength;
+    varying vec2 vUv;
+
+    void main() {
+      vec2 uv = vUv - vec2(0.5);
+      float r = length(uv);
+      uv -= uv * strength / (r*r + 0.001);
+      gl_FragColor = texture2D(tDiffuse, uv + vec2(0.5));
+    }
+  `
 };
