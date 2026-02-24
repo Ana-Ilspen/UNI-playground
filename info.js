d0d82be66c0dcbd4d7441fc6a3a9f62a65c297d1
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-24 23:06:14 +0530

    Update info.js

--- info.js
+++ info.js
@@ -1,32 +1,4 @@
-const btn =
-document.getElementById("infoBtn")
-
-const panel =
-document.getElementById("infoPanel")
-
-btn.onclick=()=>
-{
-
-panel.style.display =
-panel.style.display==="block"
-?"none":"block"
-
-panel.innerHTML=`
-
-CONTROLS
-
-WASD = thrust
-
-SHIFT = boost
-
-H = hyperspace
-
-Create planets using builder
-
-Gravity is real
-
-Land by touching planet
-
-`
-
+export function toggleInfo(){
+  const el=document.getElementById('info');
+  el.style.display = el.style.display==='none'?'block':'none';
 }
