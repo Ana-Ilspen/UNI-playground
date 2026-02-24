26439540bd650e83241d9f7df76ee0da2c60deb1
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-24 23:06:55 +0530

    Update coll.js

--- coll.js
+++ coll.js
@@ -1,16 +1,17 @@
-export function collide(ship,planet)
-{
+import { bodies } from './constants.js';
 
-const d =
-ship.position.distanceTo(
-planet.position
-)
-
-if(d < planet.radius+1)
-{
-
-ship.velocity.set(0,0,0)
-
-}
-
+export function checkCollisions(){
+  for(let a of bodies){
+    if(a.type!=="planet" && a.type!=="spacecraft") continue;
+    for(let b of bodies){
+      if(a===b) continue;
+      const dist = a.mesh.position.distanceTo(b.mesh.position);
+      const minDist = (a.mesh.geometry.parameters.radius || 1) + (b.mesh.geometry.parameters.radius || 1);
+      if(dist < minDist){
+        // Simple collision response: stop velocity
+        a.velocity.set(0,0,0);
+        b.velocity.set(0,0,0);
+      }
+    }
+  }
 }
