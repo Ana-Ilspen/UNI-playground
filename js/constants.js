3909f51aefc88b89a26498832760dfefde57300f
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-24 23:23:47 +0530

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,8 @@
-export const G = 0.05; // gravitational constant
-export const c = 300; // speed of light placeholder
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+
+export const G = 0.05;
+export const c = 300;
 export const SCALE = 1;
-export const bodies = []; // global array to store all objects
+export const bodies = [];
 export const scene = new THREE.Scene();
 export const clock = new THREE.Clock();
