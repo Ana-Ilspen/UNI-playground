f40b84941156ffbaa88b70857957b0a18b740030
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-24 23:03:52 +0530

    Update grav.js

--- grav.js
+++ grav.js
@@ -1,38 +1,16 @@
-import * as THREE from "/lib/three.module.js"
-import {G,timeScale} from "./constants.js"
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { G, bodies } from './constants.js';
 
-export function applyGravity(bodies,dt)
-{
-
-for(const a of bodies)
-{
-
-for(const b of bodies)
-{
-
-if(a===b) continue
-
-const dir =
-new THREE.Vector3()
-.subVectors(b.position,a.position)
-
-const r = dir.length()
-
-if(r<1) continue
-
-const acc =
-dir.normalize()
-.multiplyScalar(
-(G*b.mass)/(r*r)
-)
-
-a.velocity.add(
-acc.multiplyScalar(dt*timeScale)
-)
-
-}
-
-}
-
-
+export function updatePhysics(){
+  for(let a of bodies){
+    for(let b of bodies){
+      if(a===b) continue;
+      const dir=new THREE.Vector3().subVectors(b.mesh.position,a.mesh.position);
+      const dist=dir.length()+1;
+      const force=(G*a.mass*b.mass)/(dist*dist);
+      dir.normalize();
+      a.velocity.add(dir.multiplyScalar(force/a.mass));
+    }
+  }
+  for(let b of bodies) b.mesh.position.add(b.velocity);
 }
