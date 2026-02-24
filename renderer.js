32a6e05a026b8f4275b5c97051c8b5f7c9d0bc66
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-24 23:02:32 +0530

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,34 +1,9 @@
-import * as THREE from "/lib/three.module.js"
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { scene } from './constants.js';
 
-export const scene = new THREE.Scene()
+export const camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 10000000);
+camera.position.set(0,300,600);
 
-export const camera =
-new THREE.PerspectiveCamera(
-75,
-window.innerWidth/window.innerHeight,
-0.1,
-1000000
-)
-
-camera.position.set(0,20,60)
-
-export const renderer =
-new THREE.WebGLRenderer({
-canvas:document.getElementById("canvas"),
-antialias:true
-})
-
-renderer.setSize(
-window.innerWidth,
-window.innerHeight
-)
-
-const light =
-new THREE.PointLight(0xffffff,2)
-
-scene.add(light)
-
-scene.add(
-new THREE.AmbientLight(0xffffff,.3)
-
-)
+export const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(innerWidth,innerHeight);
+document.body.appendChild(renderer.domElement);
