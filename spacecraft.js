9e071c6a5f5a5b75c2d80d57038b6a0cfe2025dc
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-24 23:08:23 +0530

    Update spacecraft.js

--- spacecraft.js
+++ spacecraft.js
@@ -1,72 +1,12 @@
-import * as THREE from "/lib/three.module.js"
-import {scene,camera} from "./renderer.js"
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { scene, bodies } from './constants.js';
 
-export class Ship
-{
-
-constructor()
-{
-
-this.mass=1000
-
-this.position =
-new THREE.Vector3(0,0,50)
-
-this.velocity =
-new THREE.Vector3()
-
-this.mesh =
-new THREE.Mesh(
-
-new THREE.ConeGeometry(1,4,16),
-
-new THREE.MeshStandardMaterial({
-color:0xffffff
-})
-
-)
-
-scene.add(this.mesh)
-
-}
-
-update(input,dt)
-{
-
-const thrust = .05
-
-if(input.forward)
-this.velocity.z -= thrust
-
-if(input.back)
-this.velocity.z += thrust
-
-if(input.left)
-this.velocity.x -= thrust
-
-if(input.right)
-this.velocity.x += thrust
-
-if(input.boost)
-this.velocity.multiplyScalar(1.02)
-
-if(input.hyper)
-this.velocity.multiplyScalar(1.2)
-
-this.position.add(
-this.velocity.clone().multiplyScalar(dt)
-)
-
-this.mesh.position.copy(this.position)
-
-camera.position.lerp(
-this.position.clone().add(
-new THREE.Vector3(0,10,25)
-),.1)
-
-camera.lookAt(this.position)
-
-}
-
-
+export function createSpacecraft(pos){
+  const geo = new THREE.ConeGeometry(2,6,8);
+  const mat = new THREE.MeshStandardMaterial({color:0xff0000});
+  const mesh = new THREE.Mesh(geo,mat);
+  mesh.position.copy(pos);
+  scene.add(mesh);
+  bodies.push({mesh,mass:5,velocity:new THREE.Vector3(),type:'spacecraft'});
+  return mesh;
 }
