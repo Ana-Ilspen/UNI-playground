e26d615c1c2e4b2b7129f6b620dd1fe11896865f
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-24 23:04:11 +0530

    Update atmo.js

--- atmo.js
+++ atmo.js
@@ -1,16 +1,9 @@
-export function applyDrag(ship,planet)
-{
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 
-const d =
-ship.position.distanceTo(
-planet.position
-)
-
-if(d < planet.radius*1.2)
-{
-
-ship.velocity.multiplyScalar(.99)
-
-}
-
+export function addAtmosphere(planetMesh, radius){
+  const atmMesh=new THREE.Mesh(
+    new THREE.SphereGeometry(radius*1.15,32,32),
+    new THREE.MeshBasicMaterial({color:0x66aaff,transparent:true,opacity:0.2})
+  );
+  planetMesh.add(atmMesh);
 }
