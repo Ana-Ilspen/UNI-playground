adaf8f0c041ccd7d4181c755cff9b18cd3784091
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-25 21:55:23 +0530

    Update blackhole.js

--- js/blackhole.js
+++ js/blackhole.js
@@ -1,16 +1,54 @@
-import {scene} from "./main.js";
+import {scene,camera,renderer} from "./main.js";
 
 export function createBlackHole(radius){
 
-let geo=new THREE.SphereGeometry(radius,64,64);
+const geo=new THREE.SphereGeometry(radius,64,64);
 
-let mat=new THREE.MeshBasicMaterial({
+const mat=new THREE.ShaderMaterial({
 
-color:0x000000
+uniforms:{
+time:{value:0}
+},
+
+vertexShader:`
+
+varying vec3 vNormal;
+
+void main(){
+
+vNormal=normal;
+
+gl_Position=
+projectionMatrix*
+modelViewMatrix*
+vec4(position,1.0);
+
+}
+`,
+
+fragmentShader:`
+
+varying vec3 vNormal;
+
+void main(){
+
+float intensity=
+pow(0.6-dot(vNormal,vec3(0,0,1.0)),3.0);
+
+vec3 color=
+vec3(0.0,0.0,0.0)+
+vec3(0.3,0.0,0.5)*intensity;
+
+gl_FragColor=
+vec4(color,1.0);
+
+}
+
+`
 
 });
 
-let mesh=new THREE.Mesh(geo,mat);
+const mesh=new THREE.Mesh(geo,mat);
 
 scene.add(mesh);
 
